7c6fd5d85d2ce47ccd5061688ff52b44f7595d6f
Mateus Diniz <serv.mateus@gmail.com>
2024-01-06 16:23:56 -0300

    feat: Adicionar o modelo do post no site admin
    Agora o localhost:8000/admin tem representação visual e acesso ao banco de dados para administrar as postagens.

--- core/Posting/admin.py
+++ core/Posting/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "status", "created_on")
+    list_filter = ("status", "author")
+    search_fields = ["title", "content", "created_on"]
+    prepopulated_fields = {"slug": ("title",)}
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
